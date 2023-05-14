6d951a767da90576641c2d78bcfdb1d996fa5366
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-05-14 09:18:56 +0300

    Describe endpoint and its parameters

--- src/Controller/Api/GetFeed/v1/Controller.php
+++ src/Controller/Api/GetFeed/v1/Controller.php
@@ -6,6 +6,7 @@ use App\Service\FeedService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\View\View;
+use OpenApi\Annotations as OA;
 use Symfony\Component\Routing\Annotation\Route;
 
 class Controller extends AbstractFOSRestController
@@ -20,6 +21,11 @@ class Controller extends AbstractFOSRestController
     #[Route(path: '/api/v1/get-feed', methods: ['GET'])]
     #[QueryParam(name: 'userId', requirements: '\d+')]
     #[QueryParam(name: 'count', requirements: '\d+', nullable: true)]
+    /**
+     * @OA\Tag(name="Лента")
+     * @OA\Parameter(name="userId", description="ID пользователя", in="query", example="135")
+     * @OA\Parameter(name="count", description="ID пользователя", in="query", example="135")
+     */
     public function getFeedAction(int $userId, ?int $count = null): View
     {
         $count = $count ?? self::DEFAULT_FEED_SIZE;
