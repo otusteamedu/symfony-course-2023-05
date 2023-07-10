4df6c8f2126b10a9b9bb881204e651ab750656e0
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-07-10 15:01:48 +0300

    Use new MapRequestPayload attribute

--- src/Controller/Api/CreateUser/v5/Input/CreateUserDTO.php
+++ src/Controller/Api/CreateUser/v5/Input/CreateUserDTO.php
@@ -2,13 +2,10 @@
 
 namespace App\Controller\Api\CreateUser\v5\Input;
 
-use App\DTO\Traits\SafeLoadFieldsTrait;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class CreateUserDTO
 {
-    use SafeLoadFieldsTrait;
-
     #[Assert\NotBlank]
     #[Assert\Type('string')]
     #[Assert\Length(max: 32)]
