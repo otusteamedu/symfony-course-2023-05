otusteamedu/symfony-course-2023-02

Add DTO to annotations

src/Controller/Api/CreateUser/v5/CreateUserAction.php
@@ -3,9 +3,12 @@
 namespace App\Controller\Api\CreateUser\v5;
 
 use App\Controller\Api\CreateUser\v5\Input\CreateUserDTO;
+use App\Controller\Api\CreateUser\v5\Output\UserCreatedDTO;
 use App\Controller\Common\ErrorResponseTrait;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
@@ -18,6 +21,21 @@ class CreateUserAction extends AbstractFOSRestController
     }
 
     #[Rest\Post(path: '/api/v5/users')]
+    /**
+     * @OA\Post(
+     *     operationId="addUser",
+     *     tags={"Пользователи"},
+     *     @OA\RequestBody(
+     *         description="Input data format",
+     *         @OA\JsonContent(ref=@Model(type=CreateUserDTO::class))
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(ref=@Model(type=UserCreatedDTO::class))
+     *     )
+     * )
+     */
     public function saveUserAction(CreateUserDTO $request, ConstraintViolationListInterface $validationErrors): Response
     {
         if ($validationErrors->count()) {

otusteamedu/symfony-course-2023-05

src/Controller/Api/CreateUser/v5/CreateUserAction.php
@@ -3,9 +3,12 @@
 namespace App\Controller\Api\CreateUser\v5;
 
 use App\Controller\Api\CreateUser\v5\Input\CreateUserDTO;
+use App\Controller\Api\CreateUser\v5\Output\UserIsCreatedDTO;
 use App\Controller\Common\ErrorResponseTrait;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 
@@ -18,6 +21,21 @@ class CreateUserAction extends AbstractFOSRestController
     }
 
     #[Rest\Post(path: '/api/v5/users')]
+    /**
+     * @OA\Post(
+     *     operationId="addUser",
+     *     tags={"Пользователи"},
+     *     @OA\RequestBody(
+     *         description="Input data format",
+     *         @OA\JsonContent(ref=@Model(type=CreateUserDTO::class))
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(ref=@Model(type=UserIsCreatedDTO::class))
+     *     )
+     * )
+     */
     public function saveUserAction(#[MapRequestPayload] CreateUserDTO $request): Response
     {
         $user = $this->saveUserManager->saveUser($request);